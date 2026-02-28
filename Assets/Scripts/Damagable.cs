ff01264302bfa56b7a8dbfef70e2276f302ddec9
Diego <diegoemorales1@gmail.com>
2026-02-28 12:52:29 -0600

    add health bar

---
+++ Assets/Scripts/Damagable.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Damagable : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthbar;
+
+    public abstract float MaxHealth { get; protected set; }
+    public float Health;
+
+    public UnityEvent OnDeathEvent;
+
+    void UpdateHealthbar()
+    {
+        healthbar.Health = Health;
+        healthbar.MaxHealth = MaxHealth;
+    }
+
+    void HealHealth(float healAmount)
+    {
+        Health = Math.Min(Health + healAmount, MaxHealth);
+        UpdateHealthbar();
+    }
+
+    void DealDamage(float damage)
+    {
+        Health = Math.Max(Health - damage, 0);
+        UpdateHealthbar();
+
+        if (Health == 0)
+        {
+            OnDeathEvent.Invoke();
+        }
+    }
+
+    void Awake()
+    {
+        Health = MaxHealth;
+        UpdateHealthbar();
+
+        OnDeathEvent = new UnityEvent();
+    }
+}
