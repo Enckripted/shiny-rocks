72d941841f8f9fdd03e1165f8bc6ac3cf2409bca
juniper <orcam20098@gmail.com>
2026-02-28 17:23:01 -0600

    Add mineral destruction

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float DrillSpeed;
+    public int drillDamage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
