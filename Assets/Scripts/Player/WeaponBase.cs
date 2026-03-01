0ff0a41d4a86995810664f32a46b169c6182de54
juniper <orcam20098@gmail.com>
2026-02-28 20:30:41 -0600

    Added shot cooldown

--- Assets/Scripts/Player/WeaponBase.cs
+++ Assets/Scripts/Player/WeaponBase.cs
@@ -1,21 +1,37 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class WeaponBase : MonoBehaviour
 {
+
+    [SerializeField] private PlayerDrill playerDrill;
     [SerializeField] private GameObject fireEffect;
     [SerializeField] private Vector3 mousePos;
     [SerializeField] private Vector3 worldMousePos;
+    [SerializeField] private float shotCooldownTimer;
+    [SerializeField] private float shotCooldown = 0.5f;
 
 
     void Awake()
     {
         fireEffect = transform.Find("FireEffect").gameObject;
+        playerDrill = FindFirstObjectByType<PlayerDrill>();
     }
 
     void Update()
     {
+        if(shotCooldownTimer > 0)
+        {
+            shotCooldownTimer -= Time.deltaTime;
+            transform.Find("Sprite").gameObject.GetComponent<SpriteRenderer>().color = new Color(.25f, .25f, .25f);
+        }
+        else
+        {
+            transform.Find("Sprite").gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
+        }
+
         GameObject weaponCollider = transform.Find("WeaponCollider").gameObject;
         mousePos = Mouse.current.position.ReadValue();
         worldMousePos = Camera.main.ScreenToWorldPoint(new Vector3(
@@ -27,25 +43,28 @@ public class WeaponBase : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current.leftButton.wasPressedThisFrame && shotCooldownTimer <= 0)
         {
             Debug.Log("hit" + collision.gameObject.name);
             if (collision.gameObject.CompareTag("Enemy"))
             {
-                collision.gameObject.GetComponent<Enemy>().DealDamage(1);
+                StartCoroutine(PlayFireEffect());
+                collision.gameObject.GetComponent<Enemy>().DealDamage(playerDrill.WeaponDamage);
+                shotCooldownTimer = shotCooldown;
             }
         }   
     }
 
     void OnTriggerStay2D(Collider2D collision)
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current.leftButton.wasPressedThisFrame && shotCooldownTimer <= 0)
         {
             Debug.Log("hit" + collision.gameObject.name);
             if (collision.gameObject.CompareTag("Enemy"))
             {
                 StartCoroutine(PlayFireEffect());
-                collision.gameObject.GetComponent<Enemy>().DealDamage(5);
+                collision.gameObject.GetComponent<Enemy>().DealDamage(playerDrill.WeaponDamage);
+                shotCooldownTimer = shotCooldown;
             }
         }   
     }
