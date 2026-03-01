78ca667ffc44b3ae2b502a711f4052c4ff0d05b4
Diego <diegoemorales1@gmail.com>
2026-03-01 06:07:13 -0600

    moles on top

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -5,10 +5,21 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
 
-    public GameObject SpawnEnemy(EnemyData data, Vector3 position, Vector3 target)
+    public GameObject SpawnEnemy(EnemyData data, Vector3 position, Vector3 target, Quaternion rotation)
     {
         GameObject nObject = Instantiate(enemyPrefab);
         nObject.transform.position = position;
+        nObject.transform.rotation = rotation;
+        if (nObject.transform.rotation != Quaternion.identity)
+        {
+            Debug.Log("not equal to identity!");
+            Vector3 localScale = nObject.transform.localScale;
+            nObject.transform.localScale = new Vector3(localScale.x * -1, localScale.y, localScale.z);
+
+            Transform healthTransform = nObject.GetComponentInChildren<HealthBar>().gameObject.transform;
+            healthTransform.localScale = new Vector3(healthTransform.localScale.x, healthTransform.localScale.y * -1, healthTransform.localScale.z);
+            healthTransform.localPosition = new Vector3(0, 0.6f, 0);
+        }
         Enemy enemy = nObject.GetComponent<Enemy>();
         enemy.Initialize(data, target);
         return nObject;
