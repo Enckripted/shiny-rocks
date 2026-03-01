dd6c6337368de9e1298a31147dfce4a756649472
Diego <diegoemorales1@gmail.com>
2026-03-01 00:29:46 -0600

    depth and money counter

---
+++ Assets/Scripts/UI/StatDisplayUi.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class StatDisplayUi : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI depthText;
+    [SerializeField] private TextMeshProUGUI moneyText;
+
+    private PlayerDrill player;
+
+    void Awake()
+    {
+        player = FindFirstObjectByType<PlayerDrill>();
+    }
+
+    void Update()
+    {
+        if (moneyText != null)
+        {
+            // whole numbers only
+            double money = GameManager.instance != null ? GameManager.instance.Money : 0;
+            moneyText.text = "$" + money.ToString("N0");
+        }
+
+        if (depthText != null && player != null)
+        {
+            depthText.text = "Depth: " + player.DrillDepth.ToString("F1") + "m";
+        }
+    }
+}
