c0a51072157336d24826a3f53494b960dc1f8792
juniper <orcam20098@gmail.com>
2026-03-01 04:03:45 -0600

    added Lumps:tm:

--- Assets/Scripts/Minerals/MineralSpawner.cs
+++ Assets/Scripts/Minerals/MineralSpawner.cs
@@ -1,12 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MineralSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject mineralPrefab;
+    [SerializeField] private List<Sprite> shapeList;
 
     public GameObject SpawnMineral(MineralData data, Vector3 position)
     {
         GameObject nObject = Instantiate(mineralPrefab);
+
+        int randInt = Random.Range(1,5);
+        nObject.transform.Find("Sprite").gameObject.GetComponent<SpriteRenderer>().sprite = shapeList[randInt-1];
+
         nObject.transform.position = position;
         Mineral nMineral = nObject.GetComponent<Mineral>();
         nMineral.Initialize(data);
